11159ee2f491d938138b80aa71a1cfa5cf8e44e9
Martin <martinmcm@hotmail.es>
2021-05-11 20:52:37 +0200

    Primer commit

---
+++ src/ejercicio4/pkg10/Ejercicio410.java
@@ -0,0 +1,13 @@
+
+package ejercicio4.pkg10;
+
+
+public class Ejercicio410 {
+
+   
+    public static void main(String[] args) {
+       System.out.println(Sumador.sumar (15f,4f));
+
+    }
+    
+}
---
+++ src/ejercicio4/pkg10/Sumador.java
@@ -0,0 +1,8 @@
+package ejercicio4.pkg10;
+
+public class Sumador {
+
+    public static float sumar(float a, float b) {
+        return a + b;
+    }
+}
